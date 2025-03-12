7b68bc3b40d3b6897f7698e0bd92e955ab272e7f
aoifuji26 <luiferaoifujisawa@gmail.com>
2025-03-11 21:25:25 -0500

    nn

--- script.js
+++
@@ -1,6 +0,0 @@
-const nombres = ["NovaTech", "InnovaCorp", "SkyNet", "BlueWave", "FutureTech", "SmartVision"];
-
-function generarNombre() {
-    const randomIndex = Math.floor(Math.random() * nombres.length);
-    document.getElementById("nombre").innerText = nombres[randomIndex];
-}
